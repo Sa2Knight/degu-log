8c81e7cc663851b69574ef9d2c178cc18a5e61e7
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-01-22 08:16:36 +0000

    トップページのモック作成開始
    - bootstrapに頼り切ってヘッダーメニューとサイドメニューを作成
    - publicディレクトリを作成しそこにパスを通す
    - node-devをインストール
    - 開発過程のスクショを作成

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
+/* 静的ファイル */
+app.use('/css', express.static('public/css'));
+app.use('/js', express.static('public/js'));
+app.use('/img' , express.static('public/img'))
+
 /* サーバー起動 */
 var server = app.listen(8086, function () {
   var host = server.address().address;
