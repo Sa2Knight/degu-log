f36f625c1afb136eaf556567097fb0cb799a6fa3
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-01-23 04:16:44 +0000

    サブメニューの画面遷移を実装
    - メソッドを全てアロー関数に
    - サブメニューに応じてビューを切り替え
    - 選択中のサブメニューを強調
    - サブメニューにfont-owsomeを設定

--- public/js/app.js
+++ public/js/app.js
@@ -4,20 +4,64 @@
 
 degulog = angular.module('degulog', ['ngRoute' , 'ngAnimate']);
 degulog.config(function($routeProvider) {
+  /* ブログ */
   $routeProvider
   .when('/blog' , {
-    templateUrl: 'views/blog.html'
+    redirectTo: '/blog/list'
   })
+  .when('/blog/new' , {
+    templateUrl: 'views/blog/new.html'
+  })
+  .when('/blog/list' , {
+    templateUrl: 'views/blog/list.html'
+  })
+  .when('/blog/calendar' , {
+    templateUrl: 'views/blog/calendar.html'
+  })
+  .when('/blog/category' , {
+    templateUrl: 'views/blog/category.html'
+  })
+  /* 体重 */
   .when('/weight' , {
-    templateUrl: 'views/weight.html'
+    redirectTo: 'weight/list'
   })
+  .when('/weight/new' , {
+    templateUrl: 'views/weight/new.html'
+  })
+  .when('/weight/list' , {
+    templateUrl: 'views/weight/list.html'
+  })
+  .when('/weight/graf' , {
+    templateUrl: 'views/weight/graf.html'
+  })
+  /* 購入履歴 */
   .when('/bought' , {
-    templateUrl: 'views/bought.html'
+    redirectTo: 'bought/list'
   })
+  .when('/bought/new' , {
+    templateUrl: 'views/bought/new.html'
+  })
+  .when('/bought/list' , {
+    templateUrl: 'views/bought/list.html'
+  })
+  .when('/bought/graf' , {
+    templateUrl: 'views/bought/graf.html'
+  })
+  /* 写真 */
   .when('/photos' , {
-    templateUrl: 'views/photos.html'
+    redirectTo: 'photos/list'
   })
+  .when('/photos/new' , {
+    templateUrl: 'views/photos/new.html'
+  })
+  .when('/photos/list' , {
+    templateUrl: 'views/photos/list.html'
+  })
+  .when('/photos/tag' , {
+    templateUrl: 'views/photos/tag.html'
+  })
+  /* その他 */
   .otherwise({
-    redirectTo: '/blog'
+    redirectTo: '/blog/list'
   })
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,51 @@
 degulog.controller('MainController', ['$scope', '$location' , function($scope , $location) {
 
+  /* ベースURLを取得 */
+  let baseURL = () => ('/' + $location.path().split('/')[1]);
+
+  /* メニューごとのラベルを定義 */
+  $scope.menus = {
+    '/blog'  : 'ブログ'  ,
+    '/weight': '体重管理',
+    '/bought': '購入履歴',
+    '/photos': '写真集'  ,
+  };
+
   /* メニューごとのサブメニューを定義 */
   $scope.sideMenus = {
     '/blog': [
-      {label: '新規投稿' ,      url: '#/blog/new' ,       icon: 'fa-external-link'},
-      {label: '記事一覧' ,      url: '#/blog/list' ,      icon: 'fa-external-link'},
-      {label: 'カレンダー' ,    url: '#/blog/calendar' ,  icon: 'fa-external-link'},
-      {label: 'カテゴリ管理' ,  url: '#/blog/category' ,  icon: 'fa-external-link'},
+      {label: '記事一覧' ,      url: '#/blog/list' ,      icon: 'fa-list'},
+      {label: '新規投稿' ,      url: '#/blog/new' ,       icon: 'fa-plus-circle'},
+      {label: 'カレンダー' ,    url: '#/blog/calendar' ,  icon: 'fa-calendar'},
+      {label: 'カテゴリ管理' ,  url: '#/blog/category' ,  icon: 'fa-tags'},
     ],
     '/weight': [
-      {label: '体重登録' ,      url: '#/weight/new' ,     icon: 'fa-external-link'},
-      {label: '体重遷移' ,      url: '#/weight/graf' ,    icon: 'fa-external-link'},
+      {label: '記録一覧' ,      url: '#/weight/list' ,    icon: 'fa-list'},
+      {label: '新規登録' ,      url: '#/weight/new' ,     icon: 'fa-plus-circle'},
+      {label: '体重遷移' ,      url: '#/weight/graf' ,    icon: 'fa-line-chart'},
     ],
     '/bought': [
-      {label: '購入記録' ,      url: '#/bought/list' ,    icon: 'fa-external-link'},
-      {label: '支出記録' ,      url: '#/bought/paid' ,    icon: 'fa-external-link'},
+      {label: '購入記録' ,      url: '#/bought/list' ,    icon: 'fa-list'},
+      {label: '新規登録' ,      url: '#/bought/new' ,     icon: 'fa-plus-circle'},
+      {label: '支出記録' ,      url: '#/bought/graf' ,    icon: 'fa-bar-chart'},
     ],
     '/photos': [
-      {label: '新規投稿' ,      url: '#/photos/new' ,     icon: 'fa-external-link'},
-      {label: '写真一覧' ,      url: '#/photos/list' ,    icon: 'fa-external-link'},
-      {label: 'タグ管理' ,      url: '#/photos/tag' ,     icon: 'fa-external-link'},
+      {label: '写真一覧' ,      url: '#/photos/list' ,    icon: 'fa-picture-o'},
+      {label: '新規投稿' ,      url: '#/photos/new' ,     icon: 'fa-camera'},
+      {label: 'タグ管理' ,      url: '#/photos/tag' ,     icon: 'fa-tags'},
     ]
   };
 
+  /* 選択中のメインメニューのタイトルを取得*/
+  $scope.activeMenuTitle = () => $scope.menus[baseURL()];
+
   /* 指定したメインメニューが現在選択中か？ */
-  $scope.isActiveMenu = function(viewLocation) {
-    var active = (viewLocation === $location.path());
-    return active;
-  };
+  $scope.isActiveMenu = (viewLocation) => (viewLocation === baseURL());
+
+  /* 指定したサイドメニューが現在選択中か？ */
+  $scope.isActiveSideMenu = (viewLocation) => ('#' + $location.path()) === viewLocation;
 
   /* メニューに応じたサブメニューのリストを取得 */
-  $scope.getSideMenu = () => $scope.sideMenus[$location.path()];
+  $scope.getSideMenu = () => $scope.sideMenus[baseURL()];
 
 }]);
