db7e54c924f7a239d70375a8fafe3c63122eef7e
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-01-25 08:16:06 +0000

    体重記録の削除機能を実装
    - 仮データの順番を変更

--- public/js/weightController.js
+++ public/js/weightController.js
@@ -3,21 +3,27 @@ degulog.controller('weightController' , ['$scope' , '$routeParams', 'util' , fun
   let weight = this;
   weight.list = {};
   weight.newWeight = {date: util.formatDate(new Date() , 'YYYY/MM/DD')};
+  weight.graf = {};
 
   /* [データ] 体重記録 */
   weight.list.history = [
-    { date: '2016/09/23', pazoo: 217, may: 207 },
-    { date: '2016/09/28', pazoo: 216, may: 201 },
-    { date: '2016/10/08', pazoo: 224, may: 198 },
-    { date: '2016/10/14', pazoo: 216, may: 204 },
-    { date: '2016/11/19', pazoo: 209, may: 222 },
-    { date: '2016/12/04', pazoo: 210, may: 220 },
-    { date: '2016/12/10', pazoo: 207, may: 219 },
-    { date: '2016/12/22', pazoo: 208, may: 228 },
-    { date: '2017/01/06', pazoo: 214, may: 220 },
-    { date: '2017/01/13', pazoo: 208, may: 206 },
     { date: '2017/01/21', pazoo: 213, may: 219 },
+    { date: '2017/01/13', pazoo: 208, may: 206 },
+    { date: '2017/01/06', pazoo: 214, may: 220 },
+    { date: '2016/12/22', pazoo: 208, may: 228 },
+    { date: '2016/12/10', pazoo: 207, may: 219 },
+    { date: '2016/12/04', pazoo: 210, may: 220 },
+    { date: '2016/11/19', pazoo: 209, may: 222 },
+    { date: '2016/10/14', pazoo: 216, may: 204 },
+    { date: '2016/10/08', pazoo: 224, may: 198 },
+    { date: '2016/09/28', pazoo: 216, may: 201 },
+    { date: '2016/09/23', pazoo: 217, may: 207 },
   ];
+
+  /* [メソッド] 体重記録を削除 */
+  weight.list.remove = function(index) {
+    weight.list.history.splice(index , 1);
+  };
 
   /* [メソッド] 体重を新規登録 */
   weight.newWeight.create = function() {
@@ -27,7 +33,7 @@ degulog.controller('weightController' , ['$scope' , '$routeParams', 'util' , fun
   };
 
   /* [メソッド] 折れ線グラを生成 */
-  weight.list.createGraf = function() {
+  weight.graf.create = function() {
     // 体重記録をc3js向けのデータに変換する
     let date = ['date'] , pazoo = ['パズー'] , may = ['メイ'];
     weight.list.history.forEach(function(h) {
@@ -70,6 +76,6 @@ degulog.controller('weightController' , ['$scope' , '$routeParams', 'util' , fun
       $scope.$apply(() => $scope.weight.newWeight.date = $(this).val());
     });
     // 折れ線グラフ
-    weight.list.createGraf();
+    weight.graf.create();
   })();
 }]);
