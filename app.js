132678262ad8e8164a247e10a623c5bb0193828c
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-01-26 01:42:25 +0000

    ディレクトリ構成を一新
    - http://qiita.com/n0bisuke/items/6a79d3ee63020f771260

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var ObjectID = require('mongodb').ObjectID;
 var collection = require('./mongo');
 var app = express();
 
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/');
 app.set('view engine', 'ejs');
 
 /* トップページ */
@@ -16,11 +16,11 @@ app.get('/', function (req, res) {
 });
 
 /* 静的ファイル */
-app.use('/css', express.static('public/css'));
-app.use('/js', express.static('public/js'));
-app.use('/img' , express.static('public/img'))
-app.use('/views' , express.static('public/views'));
-app.use('/fonts' , express.static('public/fonts'));
+app.use('/styles', express.static('app/styles'));
+app.use('/scripts', express.static('app/scripts'));
+app.use('/images' , express.static('app/images'))
+app.use('/views' , express.static('app/views'));
+app.use('/fonts' , express.static('app/fonts'));
 
 /* サーバー起動 */
 var server = app.listen(8086, function () {
