ce285a5da0817de8930251ffb31ea02fffdbdfef
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-01-27 07:34:41 +0000

    セミコロン抜けを修正

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 /*
   NodeJS/express側のコアファイル
 */
-
 var express = require('express');
 var ObjectID = require('mongodb').ObjectID;
-var collection = require('./mongo');
 var app = express();
 
 app.set('views', __dirname + '/');
@@ -18,7 +16,7 @@ app.get('/', function (req, res) {
 /* 静的ファイル */
 app.use('/styles', express.static('app/styles'));
 app.use('/scripts', express.static('app/scripts'));
-app.use('/images' , express.static('app/images'))
+app.use('/images' , express.static('app/images'));
 app.use('/views' , express.static('app/views'));
 app.use('/fonts' , express.static('app/fonts'));
 
