ba23c7c72217178de63619ea1cc8e758b2e0b0ea
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-02-04 12:55:06 +0000

    モデルを別ファイルに分離

---
+++ app/scripts/models/blogModel.js
@@ -0,0 +1,51 @@
+/*
+  [モデル] 飼育日記モデル
+*/
+degulog.factory('blog' , ['$http' , function($http) {
+  let list = [];
+  return {
+    all: () => list,
+    get: (id) => list.find((e) => e.id === id),
+    getByMonth: function(year , month) {
+      let reg = new RegExp(`^${year}/0?${month}/[0-9]{2} [0-9]{2}:[0-9]{2}$`);
+      return list.filter((e) => e.datetime.match(reg));
+    },
+    load() {
+      return $http.get('/rest/blog/get').success(function(data, status, headers, config) {
+        list = data;
+      });
+    },
+    upload() {
+      $http({
+        url: "/rest/blog/post",
+        method: "POST",
+        data: list,
+        headers: {'Content-Type': 'application/json; charset=utf-8'}
+      });
+    },
+    append(newPost) {
+      list.unshift({
+        id:       new Date().getTime().toString(),
+        datetime: newPost.datetime,
+        title:    newPost.title,
+        body:     newPost.body
+      });
+      this.upload();
+    },
+    update(updatedPost) {
+      let targetIndex = list.findIndex((e) => e.id === updatedPost.id);
+      list[targetIndex] = {
+        id:       updatedPost.id,
+        datetime: updatedPost.datetime,
+        title:    updatedPost.title,
+        body:     updatedPost.body
+      };
+      this.upload();
+    },
+    remove(id) {
+      let targetIndex = list.findIndex((e) => e.id === id);
+      list.splice(targetIndex , 1);
+      this.upload();
+    },
+  };
+}]);
---
+++ app/scripts/models/weightModel.js
@@ -0,0 +1,46 @@
+/*
+  [モデル] 体重
+*/
+degulog.factory('weight' , ['$http' , function($http) {
+  let history = [];
+  return {
+    all: () => history,
+    get: (id) => history.find((h) => h.id === id),
+    append(newWeight) {
+      history.unshift({
+        id: new Date().getTime().toString(),
+        date:   newWeight.date,
+        pazoo:  newWeight.pazoo,
+        may:    newWeight.may,
+      });
+    },
+    load() {
+      return $http.get('/rest/weight/get').success(function(data, status, headers, config) {
+        history = data;
+      });
+    },
+    upload() {
+      $http({
+        url: "/rest/weight/post",
+        method: "POST",
+        data: history,
+        headers: {'Content-Type': 'application/json; charset=utf-8'}
+      });
+    },
+    update(newWeight) {
+      let targetIndex = history.findIndex((e) => e.id === newWeight.id);
+      history[targetIndex] = {
+        id:     newWeight.id,
+        date:   newWeight.date,
+        pazoo:  newWeight.pazoo,
+        may:    newWeight.may,
+      };
+      this.upload();
+    },
+    remove(id) {
+      let targetIndex = history.findIndex((h) => h.id === id);
+      history.splice(targetIndex , 1);
+      this.upload();
+    },
+  };
+}]);
