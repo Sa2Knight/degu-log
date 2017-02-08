185eb4dfb22f0fc5ae1f93aaec5f7d4717a18276
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-02-08 14:57:10 +0000

    filter.jsを追加し、テキストを一定文字数で省略出力するフィルタを実装 それを用いて、ブログ一覧の本文を省略出力

---
+++ app/scripts/filter.js
@@ -0,0 +1,12 @@
+/* テキストを指定した文字数で省略する */
+degulog.filter('abbreviate', function () {
+  return function (text, len, end) {
+    len = len || 10;
+    end = end || "…";
+    if (text.length > len) {
+      return text.substring(0, len - 1) + end;
+    } else {
+      return text;
+    }
+  };
+});
