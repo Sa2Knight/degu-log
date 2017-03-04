0234e45c5a3f1a4a29a05f14ecc907d74cf0a8ef
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-03-04 15:04:04 +0000

    写真投稿のバリデーションを申し訳程度に作成

--- app/scripts/controllers/photoController.js
+++ app/scripts/controllers/photoController.js
@@ -1,8 +1,11 @@
 /*
  * 写真の新規作成/編集画面
  */
-degulog.controller('photoEditController' , ['photoModel' , 'util' , function(photoModel) {
+degulog.controller('photoEditController' , ['photoModel' , '$scope' , function(photoModel, $scope) {
   let photo = this;
+
+  /* [フィールド] 投稿成功 */
+  photo.success = false;
 
   /* [フィールド] タグ */
   photo.tags = "";
@@ -10,14 +13,27 @@ degulog.controller('photoEditController' , ['photoModel' , 'util' , function(pho
   /* [フィールド] 添付ファイル */
   photo.file = "";
 
+  /* [フィールド] ファイル名 */
+  photo.filename = "";
+
   /* [メソッド] 投稿 */
   photo.submit = function() {
+    if ($scope.photoForm.$invalid || ! photo.file) {
+      this.success = false;
+      return;
+    }
     let photoInfo = {
       tags: photo.tags,
       file: photo.file,
       title: photo.title,
     };
     photoModel.upload(photoInfo);
+    this.success = true;
+    photo.title = "";
+    photo.filename = "";
+    photo.tags = "";
+    photo.file = null;
+    $scope.postForm.$submitted = false;
   };
 
   /* 画面初期化 */
