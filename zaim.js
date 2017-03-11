5d6cc4c205612d174c7b082128577c17335511e5
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-03-11 12:11:52 +0000

    zaimAPIキーを隠蔽

--- zaim.js
+++ zaim.js
@@ -1,11 +1,14 @@
 var Zaim = require('zaim');
+var fs = require('fs');
 
-var zaim = new Zaim({
-  consumerKey: 'be9f148fda047e7bb899cb7fea5282175bfeac97',
-  consumerSecret: '2bdc243d85f9afeeef5d0cbc30dba4e1dc563734',
-  accessToken: 'OgW7qKLqi07NTigTFicuiJbV7fLRUpiJxhn1wg83oeHnIKJiQp7zLfYbekkubA2zDd5wcXWRk6kTFI',
-  accessTokenSecret: 'KbEIZLRQEhagUIwrPWZCJplgbTNmo7b0yppFhXKdwoO26sx16uDxEf4ANhfNklISpo1g',
-});
+var keys = JSON.parse(fs.readFileSync('keys.json', 'utf8')).zaim;
+var zaimSettings = {
+  consumerKey: keys.key,
+  consumerSecret: keys.secret,
+  accessToken: keys.access_token,
+  accessTokenSecret: keys.access_token_secret
+}
+var zaim = new Zaim(zaimSettings);
 
 /* ペット関連費のジャンルID */
 var genre_id = '10203';
